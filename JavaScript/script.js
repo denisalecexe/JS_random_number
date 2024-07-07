734471da34c281d97dfd46b496418c5c1241b1c6
denisalecexe <deniszenomiteus@gmail.com>
2024-07-07 20:43:16 +0200

    fixed size

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,6 +3,6 @@ document.querySelector('button').addEventListener('click', function() {
   Math.floor(Math.random() * 1000) + 1;
 });
 
-document.querySelector('#randomNum').style.fontSize = "35px";
+document.querySelector('#randomNum').style.fontSize = "50px";
 document.querySelector('#randomNum').style.fontWeight = "800";
 document.querySelector('#randomNum').style.color = "red";
